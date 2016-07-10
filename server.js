6a4ef2e826e179fbeb0e2c68aba2ad8c37932248
Kinderley Charles <kinderleycharles@aol.com>
2016-07-10 01:36:28 -0400

    A sample example

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var app = require('express')();
-var http = require('http').createServer(app);
+var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
 
 const PORT = process.env.PORT || 5000;
+http.listen(PORT, function(){
+  console.log(`listening on: ${ PORT }`);
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -16,8 +19,4 @@ io.on('connection', function(socket){
     // console.log(msg.name + msg.status);
     io.emit('device status', msg);
   });
-});
-
-http.listen(PORT, function(){
-  console.log(`listening on: ${ PORT }`);
 });
